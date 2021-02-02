d8071ad83f336756e3aa2a48efe0646a4506c931
Looch <looch@bk.ru>
2021-02-02 14:53:34 +0300

    Finished Java 1

---
+++ src/ru/geekbrains/test/TicTacToeGame.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.test;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+
+        System.out.println(new int[][] {{1,2,7},{3,4,5}}[1][2]);
+
+    }
+
+}
